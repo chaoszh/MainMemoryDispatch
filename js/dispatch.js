acb2cef90c35b9c19890060342fae0e343c9029c
Avada Kedavra! <1499449418@qq.com>
2019-06-07 16:32:33 +0800

    first fit

--- js/dispatch.js
+++ js/dispatch.js
@@ -15,45 +15,42 @@
 
 // dispatch part
 
-var freepart=new Array();
-var busypart=new Array();
+var fp=new Array();             //freepart
+var bp=new Array();             //busypart
 var requirement=new Array();
-var si;                     //执行到第si条命令
-var stotal;                 //命令一共stotal条
-var rtotal;                 //内存分区情况
+var si;                         //执行到第si条命令
+var stotal;                     //命令一共stotal条
+var rtotal;                     //内存分区情况
 
 var button = document.getElementsByClassName("button")[0].onclick=function(){    
     // UI multipulations
     document.getElementsByClassName("button")[0].innerHTML="Next";
     document.getElementsByClassName("button")[0].onclick=respond;
-
     read();
     respond();
 }
 
-
 function read(){
     si=0;
     rtotal=1;
-    freepart.push([1,640]);
+    fp.push([1,640]);
     let str = document.getElementsByClassName("input-content")[0].value;
     requirement=str.split('\n')
     stotal=requirement.length;
 }
 
 function respond(){
-    requirement.forEach(str => {
-        respondEach(str);
-        si++;
-    });
+    respondEach(requirement[si]);
+    paint();
+    si++;
 }
 
 function respondEach(str){
-    // console.log(str);
+    // console.log(requirement);
     let req=str.split(' ');
-
+    
     req[2]=req[2].substr(0,req[2].length-1);
-
+    
     if(req[1]=='require'){
         require(req[0],Number(req[2]));
     }
@@ -63,40 +60,108 @@ function respondEach(str){
 }
 
 function require(id, space){
-    let i=0;
     let result_flag=false;
-    freepart.forEach(p=>{
-        if((p[1]-p[0]+1)>=space){
-            freepart.pop(i);
-            middle=p[0]+space-1;
-            if(middle!=p[1])freepart.push([middle,p[1]]);
-            busypart.push([p[0],p[1],id,space]);
+
+    for(let i=0;i<fp.length;i++){
+        if((fp[i][1]-fp[i][0]+1)>=space){
+            middle=fp[i][0]+space-1;
+            //update bp
+            bp.push([fp[i][0],middle,id,space]);
+            sort(bp);
+            //update fp
+            if(middle!=fp[i][1]){
+                fp[i][0]=middle+1;
+            }
+            else{
+                fp.remove(i);
+            }
+            //flag
             result_flag=true;
-            debug();
+            // debug();
         }
-        i++;
-    })
+    }
     if(result_flag==false){
-        requirement.push([id,"require",toString(space)+'K']);
+        requirement.splice(si+2,0,id+" require "+space+'K');
     }
 }
 
 function release(id, space){
-    let i=0;
-    busypart.forEach(p=>{
-        if(p[2]==id&&p[3]==space){
-            busypart.pop(i);
-            freepart.push([p[0],p[1]]);
-            debug();
+    for(let i=0;i<bp.length;i++){
+        if(bp[i][2]==id&&bp[i][3]==space){
+            fp.push([bp[i][0],bp[i][1]]);
+            sort(fp);
+            merge(fp);
+            // bp.remove(i);
+            // debug();
         }
-        i++;
-    })
+    }
+}
+
+function sort(arr){
+    for(let i=0;i<arr.length;i++){
+        let min=arr[i][0];
+        let index=i;
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[j][0]<min){
+                min=arr[j][0];
+                index=j;
+            }
+        }
+        //swap
+        let temp=arr[i][0];
+        arr[i][0]=min;
+        arr[index][0]=temp;
+    }
+}
+
+function merge(arr){
+    let post=arr[0][1];
+    for(let i=1;i<arr.length;i++){
+        let remember=arr[i][1];
+        if(arr[i][1]==post+1){
+            //connected
+            arr[i-1][1]=arr[i][1];
+            arr.remove(i);
+        }
+        post=remember;
+    }
+}
+
+
+var cGreen='linear-gradient(to right top,#249100,#a4d100)';
+var cRed='linear-gradient(to right top,#cf0643,#f74343)';
+var msdiv=document.getElementsByClassName("main-storage")[0];
+function paint(){
+    //conclude in a array p
+    let p=[];
+    for(let i=0;i<fp.length;i++){
+        p.push([fp[i][0],fp[i][1],'free']);
+    }
+    for(let i=0;i<bp.length;i++){
+        p.push([bp[i][0],bp[i][1],bp[i][2]]);
+    }
+    sort(p);
+
+    console.log(p);
+    debug();
+
+    //dom
+    let dstr='';
+    for(let i=0;i<p.length;i++){
+        let space=p[i][1]-p[i][0]+1;
+        let state=p[i][2]=='free'?'':p[i][2];
+        let color=p[i][2]=='free'?cGreen:cRed;
+        let str="<div style='height:"+space+"px; background:"+color+";'><p>"+p[i][0]+'-'+p[i][1]+' '+state+"</p></div>";
+
+        dstr+=str;
+    }
+    msdiv.innerHTML=dstr;
 }
 
 function debug(){
     console.log(
         "s_i = "+si+'\n'+
-        "freepart = "+freepart+'\n'+
-        "busypart = "+busypart
+        "freepart = "+fp+'\n'+
+        "busypart = "+bp
     );
 }
